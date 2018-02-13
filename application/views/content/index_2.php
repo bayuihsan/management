9992b5a10139ae69230fa20889c3c55a946c8d46
BRITSEL <britsel@users.noreply.github.com>
2018-02-13 11:15:39 +0700

    editorial

--- application/views/content/index_2.php
+++ application/views/content/index_2.php
@@ -3,7 +3,7 @@
 <div class="main-content">
 <div class="row">
 	<?php $level = array(1=>'Cek MSISDN', 2=>'Validasi', 3=>'TL', 4=>'Administrator', 5=>'Aktivasi / FOS', 6=>'FOS CTP', 7=>'Admin CTP'); ?>
-<div class="col-md-12 col-lg-12 col-sm-12 content-title"><h4>Selamat Datang <?php echo $this->session->userdata('nama').' ('.$level[$this->session->userdata('level')].')'?></h4></div>
+<div class="col-md-12 col-lg-12 col-sm-12 content-title"><h4>Selamat Datang <?php echo $this->session->userdata('nama').' ('.$level[$this->session->userdata('level')].') di Aplikasi HVCare System Management V2'?></h4></div>
 
 
 </div>
